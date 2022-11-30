ad348afed4e3e32325547ee5235a3df40a000f44
unknown <afif.maliki21@gmail.com>
2022-11-30 08:20:03 +0700

    adding car data

---
+++ Module2/data/datas.php
@@ -0,0 +1,27 @@
+<?php
+$datas = [
+  [
+    "name" => "Porsche 718 Spider",
+    "harga" => 90000,
+    "kursi" => 2,
+    "power" => 9000,
+    "type" => "Manual",
+    "image" => "spider"
+  ],
+  [
+    "name" => "Porsche 911 GT4",
+    "harga" => 95000,
+    "kursi" => 2,
+    "power" => 11000,
+    "type" => "Manual",
+    "image" => "911"
+  ],
+  [
+    "name" => "Porsche 911 RS",
+    "harga" => 150000,
+    "kursi" => 4,
+    "power" => 8000,
+    "type" => "Manual",
+    "image" => "RS"
+  ],
+];
